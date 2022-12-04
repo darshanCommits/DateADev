15299d66f33bfca37168de18086d38d3ea7068db
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2022-12-04 22:43:55 +0530

    MVP ready

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-addEventListener('click', change);
+function change() {
+  document.getElementById("emoji").setAttribute("src", "images/heart.png");
 
-function change () {
-    document.getElementById("emoji").setAttribute("src", "images/heart.png");
-    
-    setTimeout(function() {
-        document.getElementsByClassName("que")[0].innerText = "axha chal ab aa bol aa,";
-        document.getElementsByClassName("que")[1].innerText = "muje tumhare muh me mere bache marne hai";
-        document.getElementById("ans").classList.add("hidden");
-    }, 200);
+  setTimeout(function () {
+    document.getElementsByClassName("que")[0].innerText =
+      "axha chal ab aa bol aa,";
+    document.getElementsByClassName("que")[1].innerText =
+      "muje tumhare muh me mere bache marne hai";
+    document.getElementById("ans").classList.add("hidden");
+  }, 200);
+}
+
+let random;
+let random2;
+
+setInterval(function() {
+    random = Math.floor(Math.random() * 200) + "px";
+    random2 = Math.floor(Math.random() * 200) + "px";
+}, 10) 
+
+
+function noPos() {
+    var noButton =  document.getElementById("no");
+
+    for(let i = 0 ; i <= 99 ; i++){ 
+        noButton.style.scale = "1.1";
+        noButton.style.top = random;
+        noButton.style.left = random2;
+    }
+
+ 
 
 }
+
