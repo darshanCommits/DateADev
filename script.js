ff672ed1cde4ea9b72966da8343173009c3d964f
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2022-12-04 20:26:08 +0530

    something something

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function change () {
     document.getElementById("emoji").setAttribute("src", "images/heart.png");
     
     setTimeout(function() {
-        document.getElementsByClassName("que")[0].classList.add("invisible");
-        document.getElementsByClassName("que")[1].innerText = "axha chal ab aa bol aa, muje tumhare muh me mere bache marne hai";
+        document.getElementsByClassName("que")[0].innerText = "axha chal ab aa bol aa,";
+        document.getElementsByClassName("que")[1].innerText = "muje tumhare muh me mere bache marne hai";
         document.getElementById("ans").classList.add("hidden");
     }, 200);
 
